078a96ef995365dcb02eeb62d138b44a947a1f6a
Pam Tholan <pamelatholan@yahoo.com>
2019-08-11 18:52:27 -0400

    added links to index, view and add, added js file for form, added styling to add page

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -20,6 +20,10 @@ module.exports = function(app) {
     });
   });
 
+  app.get("/add", function(req, res) {
+    res.render("add");
+    });
+
   // Render 404 page for any unmatched routes
   app.get("*", function(req, res) {
     res.render("404");
