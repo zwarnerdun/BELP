bfb69442d0bfd9b9b369a5c9fe1122a6a63adb07
Ironam1 <ironam1@icloud.com>
2019-08-12 16:07:12 -0400

    button working to create newBar object

--- public/js/add.js
+++ public/js/add.js
@@ -1,67 +1,16 @@
-<div class="header">
-        <p>Add your favorite Happy Hour spot to our database!</p>
-    </div>
-    <form>
-      <div class="form-row">
-        <div class="form-group col-md-8">
-          <label for="barName">Name of Bar</label>
-          <input type="text" class="form-control" id="barName" placeholder="Enter your favorite watering hole!">
-        </div>
-        <div class="form-group col-md-8">
-          <label for="city">City</label>
-          <input type="text" class="form-control" id="newCity" placeholder="City">
-        </div>
-        
-          <div class="form-group col-md-6">
-              <label for="happyStart">Happy Hour Start</label>
-            
-              <input type="time" class="form-control" id="timeStart" placeholder="Happy Hour Start">
-              <span class="input-group-addon">
-                <span class="glyphicon glyphicon-time"></span>
-              </span>
-            </div>
-          </div>
-        
-        
-          <div class="form-group col-md-6">
-              <label for="happyEnd">Happy Hour End</label>
-           
-              <input type="time" class="form-control" id="timeEnd" placeholder="Happy Hour End">
-              <span class="input-group-addon">
-                <span class="glyphicon glyphicon-time"></span>
-              </span>
-            </div>
-          
-        
+$(function () {
+  $("#addBar").on("click", function(evt) {
+    evt.preventDefault();
+    // create  newBar object
+    let newBar = {
+      name: $("#barName").val().trim(),
+      timeStart: $("#timeStart").val().trim(),
+      timeEnd: $("#timeEnd").val().trim(),
+      address: $("#newCity").val().trim(),
+      price: $("#inputPrice").val().trim(),
+      rating: $("#inputRating").val().trim(),
+    };
+    console.log(newBar);
     
-        <!-- <div class="form-group col-md-7">
-          <label for="location">Lat, Long</label>
-          <input type="text" class="form-control" id="location" />
-        </div> -->
-        <div class="form-group col-md-6">
-          <label for="inputPrice">Price</label>
-          <select id="inputPrice" class="form-control">
-            <option selected="">Choose...</option>
-            <option>1</option>
-            <option>2</option>
-            <option>3</option>
-            <option>4</option>
-            <option>5</option>
-          </select>
-        </div>
-        <div class="form-group col-md-6">
-          <label for="inputRating">Rating</label>
-          <select id="inputRating" class="form-control">
-            <option selected="">Choose...</option>
-            <option>1</option>
-            <option>2</option>
-            <option>3</option>
-            <option>4</option>
-            <option>5</option>
-          </select>
-        </div>
-      
-      <button type="submit" class="btn btn-primary" id="addBar">
-        Add Bar!
-      </button>
-    </form>
+  });
+});
