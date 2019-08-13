dd72d725df45e1b3f150f8a810f5e8cfdc50c39e
Pam Tholan <pamelatholan@yahoo.com>
2019-08-12 22:12:16 -0400

    fixed map div

--- public/js/googleMap.js
+++ public/js/googleMap.js
@@ -1,7 +1,6 @@
 function initMap() {
-
-    var map = new google.maps.Map(document.getElementById('map'), {
-      zoom: 10,
-      center: {lat: 39.953, lng: -75.165}
-    });
+  var map = new google.maps.Map(document.getElementById("map"), {
+    zoom: 10,
+    center: { lat: 39.953, lng: -75.165 }
+  });
 }
